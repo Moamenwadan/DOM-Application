8cbc4f65a47cd64fc9595b22d8bbb51b70ca1471
Moamenwadan <wadanmoamen@gmail.com>
2023-10-18 13:44:23 +0300

    fifth commit

--- script.js
+++ script.js
@@ -63,3 +63,23 @@ nav.addEventListener('click', function (e) {
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+// Build a tabbled component
+const tabContainer = document.querySelector('.operations__tab-container');
+console.log(tabContainer);
+const tabsButton = document.querySelectorAll('.operations__tab');
+console.log(tabsButton);
+const tabsContent = document.querySelectorAll('.operations__content');
+console.log(tabsContent);
+
+tabContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  if (!clicked) return;
+  tabsButton.forEach(e => e.classList.remove('operations__tab--active'));
+  tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+  clicked.classList.add('operations__tab--active');
+
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
