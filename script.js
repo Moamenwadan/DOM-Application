1bcc9b5d19b8cb24b924fba1464b87037653120c
Moamenwadan <wadanmoamen@gmail.com>
2023-10-18 12:10:02 +0300

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
+// show modal
+const btnOPenModal = document.querySelector('.btn--show-modal');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const body = document.querySelector('body');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+btnOPenModal.addEventListener('click', openModal);
+btnCloseModal.addEventListener('click', closeModal);
+
+// smooth scrolling
